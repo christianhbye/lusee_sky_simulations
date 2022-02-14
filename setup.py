1decfaaeb47c88d1573e6ca1fc53b1b7934cd0aa
Christian Hellum Bye <christian.hellum.bye@gmail.com>
2022-02-14 14:24:50 -0800

    ad ipykernel as req

--- setup.py
+++ setup.py
@@ -8,21 +8,28 @@ f.close()
 setup(
     name="lusee-sky-sim",
     version="0.0.1",
-    #description="CalCalc: some catchy description",
+    # description="CalCalc: some catchy description",
     long_description=readme,
     author="Christian H. Bye",
     author_email="chbye@berkeley.edu",
     url="https://github.com/christianhbye/lusee_sky_simulations",
     packages=find_packages(),
-    install_requires=["numpy", "matplotlib", "pyuvsim", "pyuvdata", "jupyter"],
+    install_requires=[
+        "numpy",
+        "matplotlib",
+        "pyuvsim",
+        "pyuvdata",
+        "jupyter",
+        "ipykernel",
+    ],
     extras_require={"tests": "pytest", "style": ["black", "flake8"]},
     include_package_data=True,
     python_requires=">=3.6",
-    #license="MIT",
-   # classifiers=[
+    # license="MIT",
+    # classifiers=[
     #    "Intended Audience :: End Users/Desktop",
     #    "License :: OSI Approved :: MIT License",
     #    "Programming Language :: Python :: 3",
     #    "Topic :: Utilities",
-   # ],
+    # ],
 )
