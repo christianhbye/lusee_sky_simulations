264e86526a34b8f06c234ed5a89af35ec05556ab
Christian Hellum Bye <christian.hellum.bye@gmail.com>
2022-02-15 09:01:45 -0800

    fix missing imports

--- luseesky/tests/test_parse_fits.py
+++ luseesky/tests/test_parse_fits.py
@@ -1,10 +1,11 @@
-from luseesky import parse_fits as lpf
+from luseesky.utils import parse_fits as lpf
+import numpy as np
 import pytest
-import warnings
+
 
 def test_mk_linspace():
     lo, hi, step = -1, 5.5, 0.5
-    arr = lpp.mk_linspace(lo, hi, step=step)
+    arr = lpf.mk_linspace(lo, hi, step=step)
     assert np.isclose(lo, arr.min())
     assert np.isclose(hi, arr.max())
     assert np.allclose(np.diff(arr), step*np.ones_like(np.diff(arr)))
