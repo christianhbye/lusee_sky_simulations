800916eb736677ebdb49a41f174977f2ef7b485c
Christian Hellum Bye <christian.hellum.bye@gmail.com>
2022-02-16 08:57:01 -0800

    plot utilities

--- luseesky/utils/parse_fits.py
+++ luseesky/utils/parse_fits.py
@@ -1,8 +1,11 @@
 from dataclasses import dataclass, field
 from astropy.io import fits  # type: ignore
+import matplotlib.pyplot as plt
 import numpy as np
 from typing import Any
 import warnings
+
+# import to_uvbeam which makes a tmp txt file and calls read cst beam
 
 
 def mk_linspace(low: float, high: float, step: Any = 1) -> np.ndarray:
@@ -20,12 +23,19 @@ def mk_linspace(low: float, high: float, step: Any = 1) -> np.ndarray:
     num = int((high - low) // step + 1)
     return np.linspace(low, high, num=num)
 
+def Efield_to_power(efield: np.ndarray, axis: int = 3) -> np.ndarray:
+    """
+    axis: the axis representing the x,yz-components. To be summed over.
+    """
+    return np.sum(np.abs(efield)**2, axis=axis)
+
 
 @dataclass(frozen=True)
 class Beam:
     __slots__ = ["fname"]
     fname: str
     E_field: np.ndarray = field(init=False)
+    power: np.ndarray = field(init=False)
     frequencies: np.ndarray = field(init=False)
     theta: np.ndarray = field(init=False)
     phi: np.ndarray = field(init=False)
@@ -34,6 +44,7 @@ class Beam:
         simfits = fits.open(self.fname)
         header = simfits[0].header
         self.E_field = fits[0].data + 1j * fits[2].data
+        self.power = Efield_to_power(self.E_field, axis=3)
         self.frequencies = mk_linspace(
             header["freq_start"], header["freq_end"], step=header["freq_step"]
         )
@@ -45,3 +56,32 @@ class Beam:
         self.phi = mk_linspace(
             header["phi_start"], header["phi_end"], step=header["phi_step"]
         )
+        
+
+    def plot_power(self, freq: float):
+        freq_idx = np.argmin(np.abs(self.frequencies-freq))
+        plt.figure()
+        plt.imshow(self.power[freq_idx], interpolation='none')  # set extent
+        plt.colorbar()  # units of V squared
+        plt.title(f"Power at $\\nu={self.frequencies[freq_idx]}$ MHz")
+        plt.xlabel("$\\theta$ [deg]")
+        plt.ylabel("$\\phi$ [deg]")
+        plt.show()
+
+    def plot_beamcuts(self, phi: float = 0): 
+        phi_idx = np.argmin(np.abs(self.phi-phi))
+        plt.figure()
+        plt.imshow(self.power[:, :, phi_idx], interpolation='none')  # set extent
+        plt.colorbar()  # units of V squared
+        plt.title(f"Power at $\\phi={self.phi[phi_idx]}$ MHz")
+        plt.xlabel("$\\nu$ [MHz]")
+        plt.ylabel("$\\theta$ [deg]")
+        plt.show()
+
+    
+    def to_uvbeam(self, save=True):
+        uvb = mpkg.to_uvbeam(self.data)
+        if save:
+            uvb.write_beamfits()
+        # import to_uvbeam which makes a tmp txt file and calls read cst beam
+
