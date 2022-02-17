7646a6f1172220b1a3ca403c8714f020cef1e2fb
Christian Hellum Bye <christian.hellum.bye@gmail.com>
2022-02-16 20:50:09 -0800

    fix minor missing references

--- luseesky/utils/parse_fits.py
+++ luseesky/utils/parse_fits.py
@@ -3,6 +3,7 @@ from dataclasses import dataclass, field
 import matplotlib.pyplot as plt
 import numpy as np
 from pathlib import Path
+from pyuvdata import uvbeam
 from typing import Any, NoReturn
 import warnings
 
@@ -22,13 +23,14 @@ def mk_linspace(low: float, high: float, step: Any = 1) -> np.ndarray:
     num = int((high - low) // step + 1)
     return np.linspace(low, high, num=num)
 
+
 def Efield_to_power(efield: np.ndarray, axis: int = 3) -> np.ndarray:
     """
     axis: the axis representing the x,yz-components. To be summed over.
 
     returns beam in V
     """
-    return np.sqrt(np.sum(np.abs(efield)**2, axis=axis))
+    return np.sqrt(np.sum(np.abs(efield) ** 2, axis=axis))
 
 
 @dataclass(frozen=True)
@@ -57,22 +59,23 @@ class Beam:
         self.phi = mk_linspace(
             header["phi_start"], header["phi_end"], step=header["phi_step"]
         )
-        
 
     def plot_power(self, freq: float):
-        freq_idx = np.argmin(np.abs(self.frequencies-freq))
+        freq_idx = np.argmin(np.abs(self.frequencies - freq))
         plt.figure()
-        plt.imshow(self.power[freq_idx], interpolation='none')  # set extent
+        plt.imshow(self.power[freq_idx], interpolation="none")  # set extent
         plt.colorbar()  # units of V squared
         plt.title(f"Power at $\\nu={self.frequencies[freq_idx]}$ MHz")
         plt.xlabel("$\\theta$ [deg]")
         plt.ylabel("$\\phi$ [deg]")
         plt.show()
 
-    def plot_beamcuts(self, phi: float = 0): 
-        phi_idx = np.argmin(np.abs(self.phi-phi))
+    def plot_beamcuts(self, phi: float = 0):
+        phi_idx = np.argmin(np.abs(self.phi - phi))
         plt.figure()
-        plt.imshow(self.power[:, :, phi_idx], interpolation='none')  # set extent
+        plt.imshow(
+            self.power[:, :, phi_idx], interpolation="none"
+        )  # set extent
         plt.colorbar()  # units of V squared
         plt.title(f"Power at $\\phi={self.phi[phi_idx]}$ MHz")
         plt.xlabel("$\\nu$ [MHz]")
@@ -92,10 +95,8 @@ class Beam:
         else:
             raise ValueError("beam_type must be 'power' or 'efield'")
         flat_array = arr.reshape(
-                self.frequencies.size,
-                self.theta.size*self.phi.size,
-                order="F"
-            )
+            self.frequencies.size, self.theta.size * self.phi.size, order="F"
+        )
         return flat_array
 
     def _write_txt_power(self, path: str = ".", verbose: bool = False) -> str:
@@ -104,41 +105,46 @@ class Beam:
         Path.mkdir(savepath)
         if verbose:
             print(f"Saving {len(beam2d)} files to {savepath}")
-        for i, freq in enumerate(frequencies):
+        for i, freq in enumerate(self.frequencies):
             np.savetxt(
-                    savepath+f"/{freq}.txt",
-                    np.column_stack((self.theta, self.phi, beam2d[i])),
-                    header="Theta [rad] Phi [rad] Abs(V) [V] \n\n",
-                    comments="",
-                )
+                savepath + f"/{freq}.txt",
+                np.column_stack((self.theta, self.phi, beam2d[i])),
+                header="Theta [rad] Phi [rad] Abs(V) [V] \n\n",
+                comments="",
+            )
+
     def _delete_txt(path: str, verbose: bool = False) -> NoReturn:
-        Path.unlink(path+"/*.txt")
+        Path.unlink(path + "/*.txt")
         if verbose:
             print("Deleting files.")
         Path.rmdir(path)
         if verbose:
             print(f"Remove directory {path}.")
 
-    def to_uvbeam(self):
+    def to_uvbeam(self, beam_type: str = "power") -> uvbeam.UVBeam:
         if beam_type == "power":
             txtpath = self._write_txt_power()
+            txtfiles = [child.name for child in Path(txtpath).iterdir()]
+            frequencies = [float(f[:-len(".txt")]) for f in txtfiles]
             uvb = uvbeam.UVBeam()
             uvb.read_cst_beam(
-                    filename=txtpath,
-                    beam_type="power",
-                    feed_pol="x",
-                    rotate_pol=False,
-                    frequency=frequencies,
-                    telescope_name="lusee-night",
-                    feed_name="lusee",
-                    feed_version="1.0",
-                    model_name="monopole",
-                    model_version="1.0",
-                    history="003",
-                    x_orientation="north",
-                    reference_impedance=50
-                )
+                filename=txtfiles,
+                beam_type="power",
+                feed_pol="x",
+                rotate_pol=False,
+                frequency=frequencies,
+                telescope_name="lusee-night",
+                feed_name="lusee",
+                feed_version="1.0",
+                model_name="monopole",
+                model_version="1.0",
+                history="003",
+                x_orientation="north",
+                reference_impedance=50,
+            )
             self._delete_txt(txtpath)
-        return to_uvb(self.data)
-    
-
+        elif beam_type == "efield":
+            raise NotImplementedError
+        else:
+            raise ValueError("beam_type must be 'power' or 'efield'")
+        return uvb
