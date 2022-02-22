e40053bca62f702f216817210166efd6040e250b
Christian Hellum Bye <chbye@berkeley.edu>
2022-02-22 15:28:47 -0800

    minimal working file, needs improvements

--- luseesky/run_sim.py
+++ luseesky/run_sim.py
@@ -1,18 +1,25 @@
-from datetime import datetime, timedelta
-from pyuvsim import mpi, profiling, uvsim
-import time
+# import h5py
+import numpy as np
+from pyuvsim import uvsim
 
 # Will add some methods to automatically write the yaml files
 
-profiling.set_profiler(outfile_prefix="./profile", dump_raw=False)
-t0 = time.time()
-uvsim.run_uvsim("../sim_files/obsparam.yaml", return_uv=False, quiet=False)
-dt = time.time() - t0
-maxrss = mpi.get_max_mode_rss()
-rtime = str(timedelta(seconds=dt))
-if isinstance(maxrss, float):
-    print("\tRuntime: {} \n\tMaxRSS: {:.3f} GiB".format(rtime, maxrss))
-if hasattr(profiling.prof, "meta_file"):
-    with open(profiling.prof.meta_file, "a") as afile:
-        afile.write("Runtime \t {}\n MaxRSS \t {:.3f}\n".format(rtime, maxrss))
-        afile.write("Date/Time \t {}".format(str(datetime.now())))
+uvd = uvsim.run_uvsim("../sim_files/obsparam.yaml", return_uv=True, quiet=False)
+
+max_imag = np.max(np.abs(uvd.data_array.imag))
+print(f"{max_imag=}")
+
+assert np.isclose(max_imag, 0.0, atol=1e-3)
+
+uvd.data_array = uvd.data_array.real
+
+save_dict = {
+        "data": uvd.data_array,
+        "freq": uvd.freq_array,
+        "lst": uvd.lst_array
+}
+
+np.savez("./results", **save_dict)
+
+#with h5py.File("./results.h5", "w") as hf:
+#    hf.create_dataset("uvdata", data=save_dict)
