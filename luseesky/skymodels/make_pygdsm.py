0bc3dfef2adef0d42e3ca4bf8f1d881f510df6c9
Christian Hellum Bye <chbye@berkeley.edu>
2022-02-22 14:00:21 -0800

    initial commit

---
+++ luseesky/skymodels/make_pygdsm.py
@@ -0,0 +1,43 @@
+from astropy.coordinates import Galactic
+from astropy_healpix import HEALPix
+from astropy import units as u
+import numpy as np
+from pygdsm import GlobalSkyModel2016
+import pyradiosky
+
+
+REF_FREQ = 25  # MHz
+gsm = GlobalSkyModel2016(freq_unit='MHz')
+gsm.generate(REF_FREQ)
+
+NSIDE = int(np.sqrt(gsm.generated_map_data.size/12))
+NPIX = gsm.generated_map_data.size
+HPX_INDS = np.arange(NPIX)
+hp = HEALPix(nside=NSIDE, order="ring", frame=Galactic())
+assert hp.npix == NPIX
+coords = hp.healpix_to_skycoord(HPX_INDS).transform_to("icrs")
+ra, dec = coords.ra, coords.dec
+stokes = u.Quantity(np.zeros((4, 1, NPIX)), unit=u.K)
+stokes[0, 0] = gsm.generated_map_data * u.K  # set I to the GDSM Temp
+
+skymodel = pyradiosky.SkyModel()
+skymodel.Ncomponents = NPIX
+skymodel.Nfreqs = 1
+skymodel.coherency_radec = pyradiosky.utils.stokes_to_coherency(stokes)
+skymodel.component_type = "healpix"
+skymodel.dec = dec
+skymodel.history = "PyGDSM2016"
+skymodel.ra = ra
+skymodel.spectral_type = "spectral_index"
+skymodel.stokes = stokes
+
+skymodel.hpx_inds = HPX_INDS
+skymodel.hpx_order = "ring"
+skymodel.nside = NSIDE
+skymodel.reference_frequency = REF_FREQ * np.ones(NPIX) * u.MHz
+skymodel.spectral_index = -2.5 * np.ones(NPIX)
+assert skymodel.check()
+
+skymodel.healpix_to_point()
+
+skymodel.write_text_catalog("./pygdsm16_srcs.txt")
