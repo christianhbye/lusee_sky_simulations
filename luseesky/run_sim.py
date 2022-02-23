845723b45a177e5d9365c88170699eb05581458d
Christian Hellum Bye <chbye@berkeley.edu>
2022-02-22 21:10:24 -0800

    can now run on a folder of antsims

--- luseesky/run_sim.py
+++ luseesky/run_sim.py
@@ -1,23 +1,50 @@
-# type: ignore
-
 import numpy as np
+from pathlib import Path
 from pyuvsim import uvsim
+from .simsetup import gen_uvbdict, gen_obsparams
 
-# Will add some methods to automatically write the yaml files
+def run(outpath: str):
+    uvd = uvsim.run_uvsim(
+            "./sim_files.obsparam.yaml",
+            return_uv=True,
+            quiet=False
+            )
+    max_imag = np.max(np.abs(uvd.data_array.imag))
+    # print(f"{max_imag=}")
+    assert np.isclose(max_imag, 0.0, atol=1e-3), "Large imaginary part of vis"
+    uvd.data_array = uvd.data_array.real
 
-uvd = uvsim.run_uvsim("../sim_files/obsparam.yaml", return_uv=True, quiet=False)
+    save_dict = {
+            "data": uvd.data_array,
+            "freq": uvd.freq_array,
+            "lst": uvd.lst_array
+    }
 
-max_imag = np.max(np.abs(uvd.data_array.imag))
-print(f"{max_imag=}")
+    np.savez(outpath, **save_dict)
 
-assert np.isclose(max_imag, 0.0, atol=1e-3)
+if __name__ == "__main__":
+    from argparse import ArgumentParser
+    parser = ArgumentParser()
+    parser.add_argument("dir", type=str)  # path to uvbeam of antenna
+    args = parser.parse_args()
 
-uvd.data_array = uvd.data_array.real
+    DIR = args.dir
 
-save_dict = {
-        "data": uvd.data_array,
-        "freq": uvd.freq_array,
-        "lst": uvd.lst_array
-}
+    RESDIR = "results/" + DIR[len("uvbeams/AntennaSimResults/"):]
+    assert "results/Ant" in RESDIR  # checking that joining the dirname works
+    rp = Path(RESDIR)
+    if not rp.is_dir():
+        try:
+            rp.mkdir(parents=True)
+        except (FileExistsError):
+            print("The path exists but it is not a dir ...?")
 
-np.savez("results/results", **save_dict)
+    for beamfile in Path(DIR).iterdir():
+        if not beamfile.suffix == ".uvbeam":
+            continue
+        RESNAME = beamfile.name[:-len(".uvbeam")]
+        gen_uvbdict(str(beamfile))
+        gen_obsparams(str(beamfile), nsrcs=500)
+        run(RESDIR+"/"+RESNAME)
+
+
