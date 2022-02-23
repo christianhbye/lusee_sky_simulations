df0548b0303881ab21d4d6aeb69839bea789467f
Christian Hellum Bye <chbye@berkeley.edu>
2022-02-22 17:37:57 -0800

    use 200 sources

--- luseesky/skymodels/make_pygdsm.py
+++ luseesky/skymodels/make_pygdsm.py
@@ -22,7 +22,7 @@ ra, dec = coords.ra, coords.dec
 # some mock stokes params assuming Ex == Ey
 stokes = u.Quantity(np.zeros((4, 1, NPIX)), unit=u.K)
 stokes[0, 0] = gsm.generated_map_data * u.K  # set I to the GDSM Temp
-stokes[2, 0] = stokes[0, 0]  # U = I, Q=V=0
+# stokes[2, 0] = stokes[0, 0]  # U = I, Q=V=0
 print("Convert to pyradiosky SkyModel.")
 skymodel = pyradiosky.SkyModel()
 skymodel.Ncomponents = NPIX
@@ -44,9 +44,10 @@ assert skymodel.check()
 
 print("Convert to effective point sources.")
 skymodel.healpix_to_point()
-print("Filter to just the brightest sources.")
+print("Filter to just the brightest sources visible.")
 # the brightest sources has greatest stokes I
-comp_ind = np.argsort(stokes[0, 0])[-50:]  # 100 brightest
+
+comp_ind = np.argsort(stokes[0, 0])[-200:]
 skymodel.select(comp_ind, inplace=True)
 print("Save catalog to text.")
 skymodel.write_text_catalog("./pygdsm16_srcs.txt")
