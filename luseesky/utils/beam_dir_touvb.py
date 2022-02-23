9950449f1fd363e661cbd07f7c2088c53e1e1dfa
Christian Hellum Bye <chbye@berkeley.edu>
2022-02-22 19:25:19 -0800

    minor bug fixes

--- luseesky/utils/beam_dir_touvb.py
+++ luseesky/utils/beam_dir_touvb.py
@@ -1,22 +1,22 @@
 from luseesky.utils.parse_fits import Beam
 from pathlib import Path
 
-DIR = ("AntennaSimResults/003_Freq1-50MHz_Delta1MHz_AntennaLength6m_Antenna"
-        "Angle30deg_LanderHeight2m")
+DIR = ("../../AntennaSimResults/003_Freq1-50MHz_Delta1MHz_AntennaLength6m_"
+       "AntennaAngle30deg_LanderHeight2m")
 
 UVDIR = "../uvbeams/" + DIR
 uvp = Path(UVDIR)
 if not uvp.is_dir():
     try:
-        uvp.mdkir(parents=True)
+        uvp.mkdir(parents=True)
     except (FileExistsError):
         print("The path exists but it is not a dir ...?")
 
 for beamfile in Path(DIR).iterdir():
-    if beamfile.is_dir():
+    if not beamfile.suffix == ".fits":
         continue
     UVNAME = beamfile.name[:-(len(".fits"))] + ".uvbeam"
-    beam = Beam(beamfile.name)
+    beam = Beam(str(beamfile))
     uvb = beam.to_uvbeam()
     try:
         uvb.write_beamfits(UVDIR+UVNAME, clobber=False)
