fix mistake in computation of rotation matrix

luseesky/tests/test_parse_fits.py
@@ -1,4 +1,5 @@
 from luseesky.utils import parse_fits as lpf
+from luseesky.utils import coordinates as lcoords
 import numpy as np
 import pytest
 
@@ -51,43 +52,63 @@ def test_flatten():
 
 def test_sph2cart():
     E = np.array([1, 0, 0])  # radial vector along z
-    Eth, Eph = E[1:]
-    Ex, Ey, Ez = lpf.sph2cart(Eth, Eph) @ E
-    assert np.allclose(
-            lpf.sph2cart(Eth, Eph).T, np.linalg.inv(lpf.sph2cart(Eth, Eph))
-        )
+    th, ph = 0, 0
+    rot = lcoords.sph2cart(th, ph)
+    Ex, Ey, Ez = rot @ E
+    assert np.allclose(rot.T, np.linalg.inv(rot))
     assert np.isclose(Ex, 0)
     assert np.isclose(Ey, 0)
     assert np.isclose(Ez, 1)
     assert np.isclose(np.linalg.norm(E), np.linalg.norm([Ex, Ey, Ez]))
 
-    E = np.array([1, 1, 1])
-    Eth, Eph = E[1:]
-    Ex, Ey, Ez = lpf.sph2cart(Eth, Eph) @ E
-    assert np.allclose(
-            lpf.sph2cart(Eth, Eph).T, np.linalg.inv(lpf.sph2cart(Eth, Eph))
-        )
-    assert np.isclose(Ex, 1 - np.sqrt(2) / 2)
-    assert np.isclose(Ey, 1 + np.sqrt(2) / 2)
+    # x-direction
+    E = np.array([0, 0, 1])
+    th, ph = np.pi/2, -np.pi/2
+    rot = lcoords.sph2cart(th, ph)
+    Ex, Ey, Ez = rot @ E
+    assert np.allclose(rot.T, np.linalg.inv(rot))
+    assert np.isclose(Ex, 1)
+    assert np.isclose(Ey, 0)
     assert np.isclose(Ez, 0)
     assert np.isclose(np.linalg.norm(E), np.linalg.norm([Ex, Ey, Ez]))
 
 
 def test_cart2sph():
-    E = np.array([1, 1, 1])
-    Er, Eth, Eph = lpf.cart2sph(*E) @ E
-    assert np.allclose(lpf.cart2sph(*E).T, np.linalg.inv(lpf.cart2sph(*E)))
-    assert np.isclose(Er, 1 + np.sqrt(2) / 2)
-    assert np.isclose(Eth, 1 - np.sqrt(2) / 2)
+    r_hat = np.ones(3)/np.sqrt(3)  # radial unit vector @ (1, 1, 1)
+    th_hat = np.array([0, 0, -1])  # polar unit vector @ (1, 0, 0)
+    ph_hat = np.array([0, 1, 0])  # azimuthal unit vector @ (1, 0, 0)
+
+    # radial unit vector from (1, 1, 1)
+    E = r_hat
+    th, ph = np.arccos(1/np.sqrt(3)), np.pi/4
+    rot = lcoords.cart2sph(th, ph)
+    Er, Eth, Eph = rot @ E
+    assert np.allclose(rot.T, np.linalg.inv(rot))
+    assert np.isclose(Er, 1)
+    assert np.isclose(Eth, 0)
     assert np.isclose(Eph, 0)
     assert np.isclose(np.linalg.norm(E), np.linalg.norm([Er, Eth, Eph]))
 
-    E = [1, -1, 0]
-    Er, Eth, Eph = lpf.cart2sph(*E) @ E
-    assert np.allclose(lpf.cart2sph(*E).T, np.linalg.inv(lpf.cart2sph(*E)))
-    assert np.isclose(Er, np.sqrt(2))
-    assert np.isclose(Eth, 0)
+    # polar unit vector at x=1, y=0, z=0
+    E = th_hat
+    th, ph = np.pi/2, 0
+    rot = lcoords(th, ph)
+    Er, Eth, Eph = rot @ E
+    assert np.allclose(rot.T, np.linalg.inv(rot)
+    assert np.isclose(Er, 0)
+    assert np.isclose(Eth, 1)
     assert np.isclose(Eph, 0)
+    assert np.isclose(np.linalg.norm(E), np.linalg.norm([Er, Eth, Eph]))
+
+    # azimuthal unit vector at x=1, y=0, z=0
+    E = ph_hat
+    th, ph = np.pi/2, 0
+    rot = lcoords.cart2sph(th, ph)
+    Er, Eth, Eph = rot @ E
+    assert np.allclose(rot.T, np.linalg.inv(rot)
+    assert np.isclose(Er, 0)
+    assert np.isclose(Eth, 0)
+    assert np.isclose(Eph, 1)
     assert np.isclose(np.linalg.norm(E), np.linalg.norm([Er, Eth, Eph]))
 
 

luseesky/utils/parse_fits.py
@@ -7,6 +7,8 @@ from pyuvdata import uvbeam  # type: ignore
 from typing import Any, Tuple
 import warnings
 
+from .coordinates import sph2cart_array, cart2sph_array
+
 
 def mk_linspace(low: float, high: float, step: Any = 1) -> np.ndarray:
     """
@@ -32,41 +34,6 @@ def Efield_to_power(efield: np.ndarray, axis: int = 3) -> np.ndarray:
     """
 
     return np.sqrt(np.sum(np.abs(efield) ** 2, axis=axis))
-
-
-def sph2cart(theta: float, phi: float) -> np.ndarray:
-    """
-    Get the rotation matrix for transforming spherical coordinates
-    to cartesian. Note that the matrix is orthogonal so the inverse transform
-    is given by its transpose.
-    """
-    sin_th = np.sin(theta)
-    cos_th = np.cos(theta)
-    sin_ph = np.sin(phi)
-    cos_ph = np.cos(phi)
-    rot_matrix = np.array(
-        [
-            [sin_th * cos_ph, sin_th * sin_ph, cos_th],
-            [cos_th * cos_ph, cos_th * sin_ph, -sin_th],
-            [-sin_ph, cos_ph, 0],
-        ]
-    )
-    return rot_matrix
-
-
-def cart2sph(x: float, y: float, z: float) -> np.ndarray:
-    """
-    Get the rotation matrix for transforming cartesian coordinates to
-    sphericals. This is simply the transpose of the matrix that transforms
-    sphericals to cartesian.
-    """
-    r = np.sqrt(x**2 + y**2 + z**2)
-    sin_th = np.sqrt(x**2 + y**2) / r  # ok since 0 <= theta <= 180
-    cos_th = z / r
-    sin_ph = y / (r * sin_th)
-    cos_ph = x / (r * cos_th)
-    rot_matrix = sph2cart(sin_th, cos_th, sin_ph, cos_ph).T
-    return rot_matrix
 
 
 @dataclass
@@ -201,14 +168,9 @@ class Beam:
                 "E-field is already in spherical coordinates.", UserWarning
             )
         else:  # cartesian coordinates
-            Ex = self.E_field[:, :, :, 0]
-            Ey = self.E_field[:, :, :, 1]
-            Ez = self.E_field[:, :, :, 2]
-            rot_matrix_re = cart2sph(Ex.real, Ey.real, Ez.real)
-            E_real = np.einsum("ij, abcj", [rot_matrix_re, self.E_field.real])
-            rot_matrix_im = cart2sph(Ex.imag, Ey.imag, Ez.imag)
-            E_im = np.einsum("ij, abcj", [rot_matrix_im, self.E_field.imag])
-            self.E_field = E_real + 1j * E_im
+            th, ph = np.radian(self.theta), np.radian(self.phi)
+            rot_matrix = sph2cart_array(th, ph)
+            self.E_field = np.einsum("bcij, abcj", [rot_matrix, self.E_field])
             self.beam_coords = "sphericals"
 
     def to_cartesian(self):
@@ -217,16 +179,9 @@ class Beam:
                 "E-field is already in cartesian coordinates.", UserWarning
             )
         else:  # spherical coordinates
-            Er = self.E_field[:, :, :, 0]
-            Eth = self.E_field[:, :, :, 1]
-            Eph = self.E_field[:, :, :, 2]
-            th = Eth.real + 1j * Eth.imag
-            ph = Eph.real + 1j * Eph.imag
-            rot_matrix_re = sph2cart(th.real, ph.real)
-            E_real = np.einsum("ij, abcj", [rot_matrix_re, self.E_field.real])
-            rot_matrix_im = sph2cart(th.imag, ph.imag)
-            E_im = np.einsum("ij, abcj", [rot_matrix_im, self.E_field.imag])
-            self.E_field = E_real + 1j * E_im
+            th, ph = np.radian(self.theta), np.radian(self.phi)
+            rot_matrix = sph2cart_array(th, ph)
+            self.E_field = np.einsum("bcij, abcj", [rot_matrix, self.E_field])
             self.beam_coords = "cartesian"
 
     def to_uvbeam(